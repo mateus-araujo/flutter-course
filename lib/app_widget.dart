9a5ec92d96398de2be66678903c616b816282248
Mateus Araujo <mateus_1542@hotmail.com>
2021-04-02 18:05:54 -0300

    feat(login-page): uses Stack to adds a background on page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,16 +4,6 @@ import 'package:flutter_course/home_page.dart';
 import 'package:flutter_course/login_page.dart';
 
 class AppWidget extends StatelessWidget {
-  final elevatedButtonStyle = ElevatedButton.styleFrom(
-    onPrimary: Colors.black87,
-    primary: Colors.grey[300],
-    minimumSize: Size(88, 36),
-    padding: EdgeInsets.symmetric(horizontal: 16),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(2)),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
@@ -25,9 +15,6 @@ class AppWidget extends StatelessWidget {
             brightness: AppController.instance.isDarkTheme == true
                 ? Brightness.dark
                 : Brightness.light,
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: elevatedButtonStyle,
-            ),
           ),
           initialRoute: '/',
           routes: {
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,53 +13,86 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/images/logo.png')),
-                SizedBox(height: 20),
-                TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  errorMessage,
-                  style: TextStyle(color: Colors.red),
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () => _signIn(email: email, password: password),
-                  child: Text('Login'),
-                )
-              ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
             ),
+          ),
+          Container(color: Colors.black.withOpacity(0.3)),
+          _buildBody(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('assets/images/logo.png')),
+              SizedBox(height: 20),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        errorMessage,
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      SizedBox(height: 15),
+                      ElevatedButton(
+                        onPressed: () {
+                          _signIn(email: email, password: password);
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          child: Text(
+                            'Login',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
