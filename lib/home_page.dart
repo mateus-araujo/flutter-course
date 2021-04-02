2333a61b6bb01cf5dd9b0555580920d1df0051e6
Mateus Araujo <mateus_1542@hotmail.com>
2021-04-02 15:07:34 -0300

    feat(counter): simple home page with scaffold and counter

---
+++ lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Text(
+            'Counter: $counter',
+            style: TextStyle(fontSize: 20),
+          ),
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
